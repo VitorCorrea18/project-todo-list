447a9dfe7eb153077a407f8a1ea6faca9ea24bc9
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-26 18:54:38 -0400

    Finalizando os requisitos obrigatorios

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ window.onload = function () {
     const entry = document.getElementById('texto-tarefa');
     const btnAdcionar = document.getElementById('criar-tarefa');
     const list = document.getElementById('lista-tarefas');
+    const btnApaga = document.getElementById('apaga-tudo');
+    const btnApagaCompleto = document.getElementById('remover-finalizados');
 
     btnAdcionar.addEventListener('click', function () {
         let inputItem = entry.value
@@ -17,11 +19,30 @@ window.onload = function () {
             }
             event.target.classList.add('selected');
         })
+        listItem.addEventListener('dblclick', function (event) {
+            if (event.target.classList.contains('completed')) {
+                event.target.classList.remove('completed');
+            } else {
+                event.target.classList.add('completed');
+            }
+        })
+
         listItem.innerText = inputItem
         list.appendChild(listItem);
         entry.value = "";
     })
 
-    
+    btnApaga.addEventListener('click', function () {
+        let listElements = document.querySelectorAll('li');
+        for (i = 0; i < listElements.length; i += 1) {
+            listElements[i].remove();
+        }
+    })
 
+    btnApagaCompleto.addEventListener('click', function (event) {
+        let elmtCompleted = document.querySelectorAll('.completed');
+        for (i = 0; i < elmtCompleted.length; i += 1) {
+            elmtCompleted[i].remove();
+            }
+    })
 }
