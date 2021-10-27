ee39ea658cbf0409916ec8e76711723c4c095dc3
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-26 20:23:41 -0400

    VQV trabalhando para chegar aos 100%

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ window.onload = function () {
     const list = document.getElementById('lista-tarefas');
     const btnApaga = document.getElementById('apaga-tudo');
     const btnApagaCompleto = document.getElementById('remover-finalizados');
-
+    const btnSave = document.getElementById('salvar-tarefas');
+    const btnApagaSelc = document.getElementById('remover-selecionado');
+    
     btnAdcionar.addEventListener('click', function () {
         let inputItem = entry.value
         let listItem = document.createElement('li');
         listItem.className = 'list-item';
-
+        
         listItem.addEventListener('click', function (event) {
             let listElements = document.querySelectorAll('li');
             for (i = 0; i < listElements.length; i += 1) {
@@ -26,23 +28,46 @@ window.onload = function () {
                 event.target.classList.add('completed');
             }
         })
-
+        
         listItem.innerText = inputItem
         list.appendChild(listItem);
         entry.value = "";
     })
-
+    
     btnApaga.addEventListener('click', function () {
         let listElements = document.querySelectorAll('li');
         for (i = 0; i < listElements.length; i += 1) {
             listElements[i].remove();
         }
     })
-
-    btnApagaCompleto.addEventListener('click', function (event) {
+    
+    btnApagaCompleto.addEventListener('click', function () {
         let elmtCompleted = document.querySelectorAll('.completed');
         for (i = 0; i < elmtCompleted.length; i += 1) {
             elmtCompleted[i].remove();
-            }
+        }
     })
+    
+    btnSave.addEventListener('click', function(){
+        let listItens = document.querySelectorAll('li');
+        for (i = 0; i < listItens.length; i += 1) {
+            localStorage.setItem('Elemento da lista', JSON.stringify(listItens[i]));
+        }
+    })
+    
+    
+    /*if (localStorage.length > 0) {
+        
+        for (i= 0; i < localStorage.length; i += 1) {
+            let key = localStorage.key(i);
+            let savedItem = JSON.parse(localStorage[i]);
+            list.appendChild(savedItem);
+        }
+    }*/
+    
+    btnApagaSelc.addEventListener('click', function(){
+        let elemtSelec = document.querySelector('.selected');
+        elemtSelec.remove();
+    })
+
 }
