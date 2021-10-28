aa626f7df54db74aced5f98ab1e0616dcca7d236
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-27 20:33:55 -0400

    Implementando requisito 13, 100%

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 function addListItensEvents(listItem) {
   listItem.addEventListener('click', (event) => {
     const listElements = document.querySelectorAll('li');
@@ -54,40 +55,86 @@ function saveList() {
     } else { savedList.push({ task: listElements[i].innerText, status: '' }); }
   }
   localStorage.setItem('lista', JSON.stringify(savedList));
-  console.log(localStorage.getItem('lista'));
 }
 
 function recoverSavedList() {
   const savedList = JSON.parse(localStorage.getItem('lista'));
-  for (let i = 0; i < savedList.length; i += 1) {
-    const li = document.createElement('li');
-    addListItensEvents(li);
-    li.innerText = savedList[i].task;
-    li.classList.add('list-item');
-    if (savedList[i].status !== '') {
-      li.classList.add(savedList[i].status);
-      
+  if (savedList !== null) {
+    for (let i = 0; i < savedList.length; i += 1) {
+      const li = document.createElement('li');
+      addListItensEvents(li);
+      li.innerText = savedList[i].task;
+      li.classList.add('list-item');
+      if (savedList[i].status !== '') {
+        li.classList.add(savedList[i].status);
+      }
+      document.getElementById('lista-tarefas').appendChild(li);
     }
-    document.getElementById('lista-tarefas').appendChild(li);
+  }
+}
+
+function moveUp() {
+  const item = document.querySelector('.selected');
+  if (item !== null) {
+    const upperItem = item.previousElementSibling;
+    if (upperItem !== null) {
+      const itemText = item.innerText;
+      item.innerText = upperItem.innerText;
+      item.classList.remove('selected');
+      upperItem.innerText = itemText;
+      upperItem.classList.add('selected');
+      const itemCompleted = item.classList.contains('completed');
+      const upperItemCompleted = upperItem.classList.contains('completed');
+      if (itemCompleted === true && upperItemCompleted === false) {
+        upperItem.classList.add('completed');
+        item.classList.remove('completed');
+      } else if (itemCompleted === false && upperItemCompleted === true) {
+        item.classList.add('completed');
+        upperItem.classList.remove('completed');
+      }
+    }
+  }
+}
+
+function moveDown() {
+  const item = document.querySelector('.selected');
+  if (item !== null) {
+    const lowerItem = item.nextElementSibling;
+    if (lowerItem !== null) {
+      const itemText = item.innerText;
+      item.innerText = lowerItem.innerText;
+      item.classList.remove('selected');
+      lowerItem.innerText = itemText;
+      lowerItem.classList.add('selected');
+      const itemCompleted = item.classList.contains('completed');
+      const lowerItemCompleted = lowerItem.classList.contains('completed');
+      if (itemCompleted === true && lowerItemCompleted === false) {
+        lowerItem.classList.add('completed');
+        item.classList.remove('completed');
+      } else if (itemCompleted === false && lowerItemCompleted === true) {
+        item.classList.add('completed');
+        lowerItem.classList.remove('completed');
+      }
+    }
   }
 }
 
 window.onload = () => {
-  const savedList = JSON.parse(localStorage.getItem('lista'));
-  if (savedList !== null) {
-    recoverSavedList();
-  }
-
+  recoverSavedList();
   const btnAdcionar = document.getElementById('criar-tarefa');
   const btnApaga = document.getElementById('apaga-tudo');
   const btnApagaCompleto = document.getElementById('remover-finalizados');
   const btnSave = document.getElementById('salvar-tarefas');
   const btnApagaSelc = document.getElementById('remover-selecionado');
+  const btnUp = document.getElementById('mover-cima');
+  const btnDown = document.getElementById('mover-baixo');
   const entry = document.getElementById('texto-tarefa');
-
+  let selected = document.querySelector('.selected');
   btnAdcionar.addEventListener('click', () => { addListItem(entry.value); entry.value = ''; });
   btnApaga.addEventListener('click', () => { eraseAll(); });
   btnApagaCompleto.addEventListener('click', () => { eraseCompleted(); });
   btnApagaSelc.addEventListener('click', () => { eraseSelected(); });
   btnSave.addEventListener('click', () => { saveList(); });
+  btnUp.addEventListener('click', () => { moveUp(); });
+  btnDown.addEventListener('click', () => { moveDown(); });
 };
